0acd459c17c1ee6992bc13f6616394bd378c647a
Vanessa Tomaz <vanessatomaz246@gmail.com>
2023-10-27 10:33:49 -0300

    Projeto Criado

---
+++ src/test/java/com/example/workshopMongo/WorkshopMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workshopMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
